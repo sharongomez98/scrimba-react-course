0bed855c76caf5ec24772e2fb9e5cca09ef38276
Sharon Gómez <sharonrubi98@gmail.com>
2022-01-21 21:26:56 -0600

    first react practice

--- first-react/index.js
+++ first-react/index.js
@@ -1,2 +1,8 @@
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
-// ReactDOM.render(<h1>Hello World!</h1>, document.querySelector("#root"));
+ReactDOM.render(
+  <ul>
+    <li>item 1</li>
+    <li>item 2</li>
+    <li>item 3</li>
+  </ul>,
+  document.getElementById("root")
+);
