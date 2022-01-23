a23d02507388133b6375af259ccb85d9a95198d1
Sharon Gómez <sharonrubi98@gmail.com>
2022-01-23 00:26:22 -0600

    custom components part 2

--- proyect-01/index.js
+++ proyect-01/index.js
@@ -1,12 +1,20 @@
 function List() {
   return (
       <div>
+          <header>
+            <nav>
+              <img src="./react-logo.png" width="40px"/>
+            </nav>
+          </header>
           <h1>Reasons why I'm exited to be learning React</h1>
-          <ul>
+          <ol>
               <li>Is one of the most famous technologies.</li>
               <li>I want to work as a React Dev.</li>
               <li>I want to be part of the big community.</li>
-          </ul>
+          </ol>
+          <footer>
+            <small>© 2022 Gómez development. All rights reserved.</small>
+          </footer>
       </div>
   )
 }
