4f42aefee6c69de9a3a71bcb1917d0c414537e22
Sharon Gómez <sharonrubi98@gmail.com>
2022-01-24 14:33:26 -0600

    main component build

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -2,6 +2,16 @@ import React from "react"
 
 export default function Main() {
     return (
-        <h1>Main goes here</h1>
+        <main className="main">
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jorda Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+        
     );
 }
