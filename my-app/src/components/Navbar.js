0bf856fb09b542d3790084df648503093b831004
Sharon Gómez <sharonrubi98@gmail.com>
2022-01-24 12:47:56 -0600

    header component app 1

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,7 +1,12 @@
-import React from "react"
+import React from "react";
+import reactLogo from "../images/react-icon-small.png";
 
 export default function Navbar() {
-    return (
-        <h1>Navbar goes here</h1>
-    );
+  return (
+    <nav className="nav">
+      <img className="nav--logo" src={reactLogo} alt="React Logo" />
+      <h3 className="nav--logo_title">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
 }
