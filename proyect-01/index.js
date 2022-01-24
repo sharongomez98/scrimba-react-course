0738d199c0d28c0eb1bb5f0aa59fa9330c19e758
Sharon Gómez <sharonrubi98@gmail.com>
2022-01-24 03:01:30 -0600

    parent-child components

--- proyect-01/index.js
+++ proyect-01/index.js
@@ -1,22 +1,42 @@
-function List() {
+function Header() {
   return (
-      <div>
-          <header>
-            <nav>
-              <img src="./react-logo.png" width="40px"/>
-            </nav>
-          </header>
-          <h1>Reasons why I'm exited to be learning React</h1>
-          <ol>
-              <li>Is one of the most famous technologies.</li>
-              <li>I want to work as a React Dev.</li>
-              <li>I want to be part of the big community.</li>
-          </ol>
-          <footer>
-            <small>© 2022 Gómez development. All rights reserved.</small>
-          </footer>
-      </div>
-  )
+    <header>
+      <nav>
+        <img src="./react-logo.png" width="40px" />
+      </nav>
+    </header>
+  );
 }
 
-ReactDOM.render(<List />, document.getElementById("root"))
+function Footer() {
+  return (
+    <footer>
+      <small>© 2022 Gómez development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function MainComponent() {
+  return (
+    <div>
+      <h1>Reasons why I'm exited to be learning React</h1>
+      <ol>
+        <li>Is one of the most famous technologies.</li>
+        <li>I want to work as a React Dev.</li>
+        <li>I want to be part of the big community.</li>
+      </ol>
+    </div>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainComponent />
+      <Footer />
+    </div>
+  );
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
